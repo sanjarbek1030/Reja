373201e2f842cbdbfd2fcade93aaf0a2e1348e34
Sanjarbek <55908630+sanjarbek1030@users.noreply.github.com>
2026-08-26 00:32:52 +0900

    Add EJS view and update server

--- server.js
+++ server.js
@@ -13,16 +13,17 @@ app.use(express.urlencoded({extended: true}));
 
 // 3 Views code
 app.set("views", "views");
-app.set("view engines", "ejs");
+app.set("view engine", "ejs");
 
 // 4 Routing Code
-app.get("/hello", function(req, res){
-    res.end("<h1>Hello, World!</h1>");
-})
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"});
+});
 
-app.get("/gift", function(req, res){
-    res.end("<h1>Siz sovg'alar bo'limidasiz!</h1>");
-})
+app.get("/", function(req, res) {
+    res.render("harid");
+});
 
 const server = http.createServer(app);
 let PORT = 3000;
