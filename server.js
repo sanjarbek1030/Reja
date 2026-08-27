0d0acd4ea24d8ff3478895d2046c18d7884dd253
Sanjarbek <55908630+sanjarbek1030@users.noreply.github.com>
2026-08-27 20:52:55 +0900

    feat: develop author router ejpage

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 console.log("Web serverni boshlash");
-
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR", err);
+    } else {
+        user = JSON.parse(data);
+    }
+})
 
 // 1 kirish codelari
 app.use(express.static("public"));
@@ -17,9 +27,12 @@ app.set("view engine", "ejs");
 
 // 4 Routing Code
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
+    // TODO: code with db here
 });
+
+app.get("/author", (req, res) => {
+    res.render("author", {user: user});
+})
 
 app.get("/", function(req, res) {
     res.render("harid");
