3201924b29ec43a5f5b85c4e9a355df3bdf1bac2
Sanjarbek <55908630+sanjarbek1030@users.noreply.github.com>
2026-08-30 15:07:51 +0900

    feat: added function into train.js

--- train.js
+++ train.js
@@ -1,1 +1,35 @@
-console.log("Train task ishga tushdi!");
+// 1. Function Declaration
+function countLetters(str, char) {
+    let count = 0;
+    for (const letter of str) {
+      if (letter === char) {
+        count = count + 1;
+      }
+    }
+    return count;
+  }
+  
+  // 2. Function Expression
+  const countLetters2 = function(str, char) {
+    let count = 0;
+    for (const letter of str) {
+      if (letter === char) {
+        count += 1;
+      }
+    }
+    return count;
+  }
+  
+  // 3. Arrow Function
+  const countLetters3 = (str, char) => {
+    let count = 0;
+    for (const letter of str) {
+      if (letter === char) count += 1;
+    }
+    return count;
+  }
+  
+  console.log(countLetters("sanjar", "a"));
+  console.log(countLetters2("boburbek", "b"));
+  console.log(countLetters3("mexanizatsiyalashtirilganligidanmikina", "a"));
+  
