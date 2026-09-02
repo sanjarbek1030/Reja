d62619de145f55a200097baea1e8be6cfee22169
Sanjarbek <55908630+sanjarbek1030@users.noreply.github.com>
2026-09-02 19:39:35 +0900

    feat: connecting to mongodb

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const fs = require("fs");
+
+// calling MongoDB
+const db = require("./server").db();
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR", err);
+    } else {
+        user = JSON.parse(data);
+    }
+})
+
+// 1 kirish codelari
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2 sessions code
+
+// 3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing Code
+app.post("/create-item", (req, res) => {
+    // TODO: code with db here
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", {user: user});
+})
+
+app.get("/", function(req, res) {
+    res.render("reja");
+});
+
+module.exports = app;
+
