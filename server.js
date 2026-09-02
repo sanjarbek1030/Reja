a21770b5780035a83eeabe3115782efe91cd326c
Sanjarbek <55908630+sanjarbek1030@users.noreply.github.com>
2026-09-02 21:19:41 +0900

    feat: fixed mongodb

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const mongodb = require("mongodb");
 
 let db;
-const connectionString = "mongodb+srv://sanjarbekjumaboyev95_db_user:TmIaEGyM7FqAMWsO@cluster0.fkars7v.mongodb.net/?Reja=Cluster0";
+const connectionString = "mongodb+srv://sanjarbekjumaboyev95_db_user:TmIaEGyM7FqAMWsO@cluster0.fkars7v.mongodb.net/Reja?=Cluster0";
 
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true,},
     (err, client) => {
